26bc6d0cb8886fe1c14fae92f9371dcc2d82dadc
Raymundosalas <128091595+Raymundosalas@users.noreply.github.com>
2026-03-03 17:48:43 -0600

    Add PlayerAnimation script for character animations

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(rb.linearVelocity.x));
+        animator.SetFloat("VerticalVelocity", rb.linearVelocity.y);
+    }
+}
